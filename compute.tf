05e84c87b766da70d099143c70c1be8420355cbf
radecpro <radecki94@gmail.com>
2025-01-12 18:27:47 +0100

    Added VM instance and firewall rule

---
+++ compute.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "admin_vm" {
+  name         = "${local.naming_prefix}-admin-vm"
+  zone         = var.gcp_zone
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.custom-vpc.id
+    subnetwork = google_compute_subnetwork.vpc-subnet.id
+  }
+}
--- network.tf
+++ network.tf
@@ -10,3 +10,17 @@ resource "google_compute_subnetwork" "vpc-subnet" {
   network                  = google_compute_network.custom-vpc.id
   private_ip_google_access = true
 }
+
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "${local.naming_prefix}-fw-allow-ssh"
+  network = google_compute_network.custom-vpc.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  priority = "995"
+  // Connection via IAP for VMs without external IP
+  source_ranges = ["35.235.240.0/20"]
+}
