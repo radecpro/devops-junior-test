03511166e08a2f3f3d753b4710e2bc23e9bb09f7
radecpro <radecki94@gmail.com>
2025-01-12 17:50:08 +0100

    Removed files from bucket

--- storage.tf
+++ storage.tf
@@ -6,10 +6,3 @@ resource "google_storage_bucket" "website_content" {
 
   uniform_bucket_level_access = true
 }
-
-resource "google_storage_bucket_object" "website_files" {
-  for_each = fileset("${path.module}/website", "**")
-  name     = each.value
-  bucket   = google_storage_bucket.website_content.name
-  source   = "${path.module}/website/${each.value}"
-}
