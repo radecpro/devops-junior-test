7664a1b922ec3996df47a798984e45145d9648d6
radecpro <radecki94@gmail.com>
2025-01-13 21:12:35 +0100

    Removed NAT

--- network.tf
+++ network.tf
@@ -36,26 +36,3 @@ resource "google_compute_firewall" "allow-http" {
 
   source_ranges = ["0.0.0.0/0"] # Or a more restrictive range
 }
-
-resource "google_compute_router" "router" {
-  name    = "${local.naming_prefix}-router"
-  region  = google_compute_subnetwork.vpc-subnet.region
-  network = google_compute_network.custom-vpc.id
-
-  bgp {
-    asn = 64514
-  }
-}
-
-resource "google_compute_router_nat" "nat" {
-  name                               = "${local.naming_prefix}-router-nat"
-  router                             = google_compute_router.router.name
-  region                             = google_compute_router.router.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-
-  log_config {
-    enable = true
-    filter = "ERRORS_ONLY"
-  }
-}
